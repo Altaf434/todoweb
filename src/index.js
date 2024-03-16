1b20a2b7aca24ad91147e8e651ea994f5872e9f5
Altaf434 <148451976+Altaf434@users.noreply.github.com>
2024-03-17 04:41:16 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Search from './search.js';
+import Search from 'search.js';
 import axios from 'axios';
 ReactDOM.render(
  <div>
